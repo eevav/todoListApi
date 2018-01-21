2f09dd374a397dd2e07533ee6c89efd6e2df941f
eevav <eevavilson@gmail.com>
2018-01-21 22:40:01 +0200

    redirect for wrong route

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ app.listen(port);
 
 
 console.log('todo list RESTful API server started on: ' + port);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
